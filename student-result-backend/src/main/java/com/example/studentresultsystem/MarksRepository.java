79d969900beb6b87e8bc7cc409c93bce4d7b17e8
Swapnil Pawar <swapnildpawar011@gmail.com>
2026-05-23 11:02:48 +0530

    remove all files

--- student-result-backend/src/main/java/com/example/studentresultsystem/MarksRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.studentresultsystem;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MarksRepository extends JpaRepository<Marks, Long> {
-    Marks findByStudentId(Long studentId);
-}
